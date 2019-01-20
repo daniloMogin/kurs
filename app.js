eda10eca4295fc104e7a259841327f47011c0722
Danilo Mogin <danilo.mogin@gmail.com>
2019-01-20 22:10:42 +0100

    dodato svasta

--- app.js
+++ app.js
@@ -39,6 +39,44 @@ angularjsKurs.controller(
         snippet: 'The Next, Next Generation tablet.'
       }
     ];
+    $scope.cat = [
+      {
+        img: 'https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg',
+        name: 'funny'
+      },
+      {
+        img: 'https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg',
+        name: 'funny'
+      }
+    ]
     $scope.name = 'DOIT';
+    $scope.count = 0;
+    $scope.countFunc = 0;
+    $scope.input = 'AngularJS';
+
+    $scope.increment = () => {
+      $scope.countFunc++;
+    };
+
+    $scope.decrement = () => {
+      $scope.countFunc--;
+    };
+
+    $scope.firstName;
+    $scope.lastName;
+    $scope.age;
+    $scope.email;
+
+    $scope.submit = () => {
+      const obj = {
+        name: $scope.firstName,
+        lastName: $scope.lastName,
+        age: $scope.age,
+        email: $scope.email
+      }
+      console.log(`Personal data`);
+      console.log(obj);
+      
+    }
   }
 );
