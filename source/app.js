993d2f44037bf74370d22b8304b07d4b0b2161c7
Danilo Mogin <danilo.mogin@enlightit.rs>
2019-01-24 16:15:39 +0100

    posle  kursa

--- source/app.js
+++ source/app.js
@@ -3,80 +3,45 @@ var angularjsKurs = angular.module('angularjsKurs', []);
 
 // Definisanje `AngularJSKursController` kontrolera u `angularjsKurs` modulu
 angularjsKurs.controller(
-  'AngularJSKursController',
-  function AngularJSKursController($scope) {
-    $scope.phones = [
-      {
-        name: 'Nexus S',
-        snippet: 'Fast just got faster with Nexus S.'
-      },
-      {
-        name: 'Motorola XOOM™ with Wi-Fi',
-        snippet: 'The Next, Next Generation tablet.'
-      },
-      {
-        name: 'MOTOROLA XOOM™',
-        snippet: 'The Next, Next Generation tablet.'
-      }
-    ];
-    $scope.name = 'world';
-  }
-);
-angularjsKurs.controller(
-  'AngularJSKursControllerNoviScope',
-  function AngularJSKursController($scope) {
-    $scope.phonesNewScope = [
-      {
-        name: 'Nexus S DOIT',
-        snippet: 'Fast just got faster with Nexus S.'
-      },
-      {
-        name: 'Motorola XOOM™ with Wi-Fi DOIT',
-        snippet: 'The Next, Next Generation tablet.'
-      },
-      {
-        name: 'MOTOROLA XOOM™ DOIT',
-        snippet: 'The Next, Next Generation tablet.'
-      }
-    ];
-    $scope.cat = [
-      {
-        img: 'https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg',
-        name: 'funny'
-      },
-      {
-        img: 'https://img-9gag-fun.9cache.com/photo/aM23KQV_460s_v1.jpg',
-        name: 'funny'
-      }
-    ]
-    $scope.name = 'DOIT';
-    $scope.count = 0;
-    $scope.countFunc = 0;
-    $scope.input = 'AngularJS';
+    'AngularJSKursController',
+    function AngularJSKursController($scope) {
+        $scope.phones = [
+            {
+                name: 'Nexus S',
+                snippet: 'Fast just got faster with Nexus S.'
+            },
+            {
+                name: 'Motorola XOOM™ with Wi-Fi',
+                snippet: 'The Next, Next Generation tablet.'
+            },
+            {
+                name: 'MOTOROLA XOOM™',
+                snippet: 'The Next, Next Generation tablet.'
+            }
+        ];
+        $scope.name = 'world';
+        $scope.count = 10;
 
-    $scope.increment = () => {
-      $scope.countFunc++;
-    };
+        $scope.increment = () => {
+            $scope.count++;
+        }
 
-    $scope.decrement = () => {
-      $scope.countFunc--;
-    };
+        $scope.firstName = 'Pera';
+        $scope.lastName = 'Peric';
+        $scope.age = 20;
+        $scope.email = 'pera.peric@email.com';
+        
+        $scope.submit = () => {
+            const userObj = {
+                name: $scope.firstName,
+                lastName: $scope.lastName,
+                age: $scope.age,
+                email: $scope.email
+            }
 
-    $scope.firstName;
-    $scope.lastName;
-    $scope.age;
-    $scope.email;
-
-    $scope.submit = () => {
-      const obj = {
-        name: $scope.firstName,
-        lastName: $scope.lastName,
-        age: $scope.age,
-        email: $scope.email
-      }
-      console.log(`Personal data`);
-      console.log(obj);
-      
+            console.log(`userObj`);
+            console.log(userObj);
+            
+        }
     }
-  }
 );
