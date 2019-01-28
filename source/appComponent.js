4f6cc9515544540091578de836e3235956b5c760
Danilo Mogin <danilo.mogin@enlightit.rs>
2019-01-28 17:09:14 +0100

    added custom filter

--- source/appComponent.js
+++ source/appComponent.js
@@ -1,19 +1,21 @@
-angular.module('angularjsKurs').component('convertCtrl', {
-    templateUrl: 'convertComp.html',
-    controller: function ConvertCtrlController($scope) {
-        $scope.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.'
-            },
-            {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-            },
-            {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
-            }
-        ];
-    }
-});
+angular.module('angularjsKurs')
+    .component('convertCtrl', {
+        templateUrl: 'convertComp.html',
+        controller: function ConvertCtrlController($scope) {
+            $scope.customFilter = 'angularjsKurs';
+            $scope.phones = [
+                {
+                    name: 'Nexus S',
+                    snippet: 'Fast just got faster with Nexus S.'
+                },
+                {
+                    name: 'Motorola XOOM™ with Wi-Fi',
+                    snippet: 'The Next, Next Generation tablet.'
+                },
+                {
+                    name: 'MOTOROLA XOOM™',
+                    snippet: 'The Next, Next Generation tablet.'
+                }
+            ];
+        }
+    });
